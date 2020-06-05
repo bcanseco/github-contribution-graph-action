245b80f3d0b3b4526cacf4b8f686d1ab2b1cd486
Borja Canseco <borj@cans.eco>
2020-06-04 22:48:08 -0500

    refactor: use dotenv-extended and auto-parse

--- src/index.js
+++ src/index.js
@@ -1,56 +1,43 @@
-import fs from 'fs/promises';
-import git from 'simple-git/promise';
-import subDays from 'date-fns/fp/subDays';
+import autoParse from 'auto-parse';
+import dotenv from 'dotenv-extended'
 import getUnixTime from 'date-fns/fp/getUnixTime';
 import fromUnixTime from 'date-fns/fp/fromUnixTime';
+import subDays from 'date-fns/fp/subDays';
 import isWeekend from 'date-fns/fp/isWeekend';
+import fs from 'fs/promises';
+import git from 'simple-git/promise';
 import {getRandomInt} from './random';
 
-const {
-  GITHUB_ACTOR,
-  GITHUB_REPOSITORY,
-  GITHUB_TOKEN,
-  GIT_EMAIL,
-  GIT_BRANCH = 'master',
-  GIT_HOST = 'github.com',
-  GIT_COMMIT_MESSAGE = 'chore(actions): empty commit for contribution graph',
-  GIT_SSH_COMMAND = 'ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no',
-
-  ORIGIN_TIMESTAMP = getUnixTime(new Date()),
-  MAX_DAYS = 1,
-  MIN_COMMITS_PER_DAY = 1,
-  MAX_COMMITS_PER_DAY = 1,
-  INCLUDE_WEEKDAYS = true,
-  INCLUDE_WEEKENDS = true,
-  FORCE_PUSH = false,
-} = process.env;
-
-const repoPath = `https://${GITHUB_ACTOR}:${GITHUB_TOKEN}@${GIT_HOST}/${GITHUB_REPOSITORY}`;
+const env = autoParse({
+  ORIGIN_TIMESTAMP: process.env.ORIGIN_TIMESTAMP || getUnixTime(new Date()),
+  ...dotenv.load({errorOnMissing: true, includeProcessEnv: true}),
+});
+const repoPath = `https://${env.GITHUB_ACTOR}:${env.GITHUB_TOKEN}@${env.GIT_HOST}/${env.GITHUB_REPOSITORY}`;
 const localPath = './clone';
 const secondLine = 'Committed via https://github.com/marketplace/actions/autopopulate-your-contribution-graph';
-const dayOffsets = [...Array(Number(MAX_DAYS)).keys()];
-const originDay = fromUnixTime(ORIGIN_TIMESTAMP);
+const dayOffsets = [...Array(env.MAX_DAYS).keys()];
+const originDay = fromUnixTime(env.ORIGIN_TIMESTAMP);
 
 await fs.mkdir(localPath);
 
-if (JSON.parse(FORCE_PUSH)) {
+if (env.FORCE_PUSH) {
   await git(localPath).init();
 } else {
-  await git().clone(repoPath, localPath, ['--single-branch', '-b', GIT_BRANCH]);
+  await git().clone(repoPath, localPath, ['--single-branch', '-b', env.GIT_BRANCH]);
 }
 
-await git(localPath).env({GIT_SSH_COMMAND});
-await git(localPath).addConfig('user.name', GITHUB_ACTOR);
-await git(localPath).addConfig('user.email', GIT_EMAIL);
+await git(localPath).env({GIT_SSH_COMMAND: env.GIT_SSH_COMMAND});
+await git(localPath).addConfig('user.name', env.GITHUB_ACTOR);
+await git(localPath).addConfig('user.email', env.GIT_EMAIL);
 
 await dayOffsets
   .map((dayOffset) => subDays(dayOffset, originDay))
-  .filter((day) => !(!JSON.parse(INCLUDE_WEEKENDS) && isWeekend(day)))
-  .filter((day) => !(!JSON.parse(INCLUDE_WEEKDAYS) && !isWeekend(day)))
+  .filter((day) => !(!env.INCLUDE_WEEKENDS && isWeekend(day)))
+  .filter((day) => !(!env.INCLUDE_WEEKDAYS && !isWeekend(day)))
   .map((/** @type {Date} */ day) => {
-    const commitsToMake = getRandomInt(MIN_COMMITS_PER_DAY, MAX_COMMITS_PER_DAY);
+    const commitsToMake = getRandomInt(env.MIN_COMMITS_PER_DAY, env.MAX_COMMITS_PER_DAY);
     return [...Array(commitsToMake)].map((_, i) => async () => {
-      const {commit: sha} = await git(localPath).commit([GIT_COMMIT_MESSAGE, secondLine], {
+      const {commit: sha} = await git(localPath).commit([env.GIT_COMMIT_MESSAGE, secondLine], {
         '--allow-empty': null,
         '--date': `format:iso8601:${day.toISOString()}`,
       });
@@ -60,4 +47,4 @@ await dayOffsets
   .flat()
   .reduce((commitPromises, nextPromise) => commitPromises.then(nextPromise), Promise.resolve());
 
-await git(localPath).push(repoPath, GIT_BRANCH, JSON.parse(FORCE_PUSH) && {'--force': null});
+await git(localPath).push(repoPath, env.GIT_BRANCH, env.FORCE_PUSH && {'--force': null});
